cefe0421d3ca5151795d11bcab35cc8b7bee3d09
jason <jasonok@protonmail.com>
2016-10-06 20:25:22 +0200

    navigate between loans

--- app/assets/javascripts/dashboardLoad.js
+++ app/assets/javascripts/dashboardLoad.js
@@ -1,18 +1,77 @@
-    function dashboardLoad() {
-        console.log("in dashboard load");
-        var jsonData = sessionStorage.getItem("jsondata");
-        var jsonData = JSON.parse(jsonData);
-        var loan1 = jsonData["loan1"];
-        var loan1_name = loan1["name"];
-        var loan1_principal = loan1["principal"];
-        var loan1_interest_rate = loan1["interest rate"];
-        var loan1_compounds_per_year = loan1["compounds per year"];
-        var loan1_years = loan1["years of loan"];
-        var loan1_maturity_date = loan1["maturity date"];
+function dashboardLoad() {
+    createLoanList();
+    dashboardWrite(1);
+}
 
-        $('#principal').val(loan1_principal);
-        $('#interest_rate').val(loan1_interest_rate);
-        $('#compounds_per_year').val(loan1_compounds_per_year);
-        $('#loan_years').val(loan1_years);
-        $('#terminal_date').val(loan1_maturity_date);
+function createLoanList() {
+    console.log("in dashboard load");
+    var jsonData = sessionStorage.getItem("jsondata");
+    var jsonData = JSON.parse(jsonData);
+    //Load the JSON object into an array
+    var loanArray = [];
+    for (var key in jsonData) {
+        if (jsonData.hasOwnProperty(key)) {
+            var val = jsonData[key]
+            loanArray.push(val);
+        }
     }
+
+    for (var j = 0; j < loanArray.length; j++) {
+        $('#loanList').append($("<li>", {
+            id: j + 1,
+            text: loanArray[j]["name"]
+        }));
+    }
+}
+
+function dashboardWrite(index) {
+    console.log("in dashboardWrite");
+    var jsonData = sessionStorage.getItem("jsondata");
+    var jsonData = JSON.parse(jsonData);
+
+    var loanArray = [];
+    for (var key in jsonData) {
+        if (jsonData.hasOwnProperty(key)) {
+            var val = jsonData[key]
+            loanArray.push(val);
+        }
+    }
+
+    var i = index;
+    var activeLoan = function(i) {
+        i = i - 1; //to account for index 0 being first position
+        return loanArray[i];
+    }
+
+    $('#principal').val(activeLoan(i)["principal"]);
+    $('#interest_rate').val(activeLoan(i)["interest rate"]);
+    $('#compounds_per_year').val(activeLoan(i)["compounds per year"]);
+    $('#loan_years').val(activeLoan(i)["years of loan"]);
+    $('#terminal_date').val(activeLoan(i)["maturity date"]);
+}
+
+$(document).on('click', '#1', function() {
+    dashboardWrite(this["id"]);
+    updateChart();
+    basicMath();
+});
+$(document).on('click', '#2', function() {
+    dashboardWrite(this["id"]);
+    updateChart();
+    basicMath();
+});
+$(document).on('click', '#3', function() {
+    dashboardWrite(this["id"]);
+    updateChart();
+    basicMath();
+});
+$(document).on('click', '#4', function() {
+    dashboardWrite(this["id"]);
+    updateChart();
+    basicMath();
+});
+$(document).on('click', '#5', function() {
+    dashboardWrite(this["id"]);
+    updateChart();
+    basicMath();
+});
