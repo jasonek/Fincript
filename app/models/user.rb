6adc1d4194f7b37d07382da9b33644bcbd9d903c
Jason Kucharski <jasonok@protonmail.ch>
2017-01-02 13:29:17 -0800

    modify gemfile and database.yml to run sqlite3 by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # include Encryption
-  # attr_encrypted :email, key: :encryption_key, algorithm: 'aes-256-gcm', mode: :per_attribute_iv, encode: true, encode_iv: true, encode_salt: true
+  # attr_encryptor :email, key: ENV["EMAIL_ENC_ATTR"]#, algorithm: 'aes-256-gcm', mode: :per_attribute_iv, encode: true, encode_iv: true, encode_salt: true
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, length: {:minimum => 5, :maximum => 99}, format: { with: /.+@.+\..+/i } #all I check for is the @ and a dot
