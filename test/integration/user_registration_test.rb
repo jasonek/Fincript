4fbdbc0c8740c1ffece95f4033f3a75cd8e63b6f
Jason Kucharski <jasonok@protonmail.ch>
2017-01-09 09:43:42 -0800

    add integration test for valid user registrations

--- test/integration/user_registration_test.rb
+++ test/integration/user_registration_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do
+  test "invalid registration information" do
     get register_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { email: "user@invalid", password: "foo", password_confirmation: "bar" } }
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test "valid registration information" do
     get register_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { email: "valid@valid.com", password: "abc123", password_confirmation: "abc123" } }
